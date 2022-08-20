8533287d9c85e762d35a5637949e3e12660a25ef
Dan Cornell <dancrnll@yahoo.com>
2022-08-19 19:36:51 -0700

    Lesson 4 - response object

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    // console.log(req);
     console.log(req.url, req.method);
+
+    // set header contenst type
+    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>hello, ninjas</p>');
+    res.write('<p>hello again, ninjas</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
