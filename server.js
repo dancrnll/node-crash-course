2d7cf4244b1cfa1f304cd4f2a3b3592fd093e8d7
Dan Cornell <dancrnll@yahoo.com>
2022-08-19 19:43:39 -0700

    Lesson 4 - returning HTML pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(req);
@@ -8,10 +9,17 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello, ninjas</p>');
-    res.write('<p>hello again, ninjas</p>');
-    res.end();
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
 });
 
 server.listen(3000, 'localhost', () => {
